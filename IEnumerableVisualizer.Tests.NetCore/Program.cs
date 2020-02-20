be601bbaca8d0989bf8528004b13bd44e0919dee
cyb3rm4ge <daniel.edward.ohm@gmail.com>
2020-02-19 18:11:13 -0800

    update preview

--- IEnumerableVisualizer.Tests.NetCore/Program.cs
+++ IEnumerableVisualizer.Tests.NetCore/Program.cs
@@ -87,19 +87,6 @@ namespace IEnumerableVisualizer.Tests.NetCore
         }
     }
 
-    class CustomObject
-    {
-        public string Field1;
-        public string Field2;
-        public string Field3;
-        public string Field4;
-        public int Error { get { throw new Exception("test"); } }
-        public string Property1 { get; set; }
-        public string Property2 { get; set; }
-        public string Property3 { get; set; }
-        public string Property4 { get; set; }
-    }
-
     class CustomObjectNoFields
     {
         public string Property1 { get; set; }
